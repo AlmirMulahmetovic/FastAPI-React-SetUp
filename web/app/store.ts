812f821e63729111a05dfd875efbfcb96b880dde
Nermin Hrustic <nermin.hrustic.ets@gmail.com>
2022-10-22 10:11:32 +0200

    [TEAM1-16] Enamble CORS

---
+++ web/app/store.ts
@@ -0,0 +1,20 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import counterSlice from "./features/counter/counterSlice";
+import pokemonSlice from "./features/pokemon/pokemonSlice";
+import { pokemonApi } from "./features/pokemonApi/pokemonApi";
+import { api } from "./services/apiService";
+
+export const store = configureStore({
+  reducer: {
+    counter: counterSlice,
+    pokemon: pokemonSlice,
+    [pokemonApi.reducerPath]: pokemonApi.reducer,
+    [api.reducerPath]: api.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(pokemonApi.middleware).concat(api.middleware),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
